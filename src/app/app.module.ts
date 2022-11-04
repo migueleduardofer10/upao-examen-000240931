8c9d223387d1955361be194ef72ea4f26a094113
Miguel Eduardo <migueleduardofer10@gmail.com>
2022-11-04 14:06:57 -0500

    registro de datos de espacios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './homee/home/home.component';
+import { NewEspacioComponent } from './new-espacio/new-espacio/new-espacio.component';
+import { ListEspaciosComponent } from './list-espacios/list-espacios/list-espacios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewEspacioComponent,
+    ListEspaciosComponent
   ],
   imports: [
     BrowserModule,
